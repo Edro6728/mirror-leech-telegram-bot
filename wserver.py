37884809fa3223763613809e3d49bacbf19a9000
ElJoker63 <eljoker630@gmail.com>
2022-01-03 22:20:35 -0500

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -684,7 +684,308 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/ElJoker63/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/ElJoker63'>ElJoker63</a></h1>", content_type="text/html")
+    return web.Response(text="""<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Torrent File Selector</title>
+    <link rel="icon" href="https://telegra.ph/file/cc06d0c613491080cc174.png" type="image/jpg">
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+      crossorigin="anonymous"
+    ></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+<style>
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Ubuntu", sans-serif;
+    list-style: none;
+    text-decoration: none;
+    outline: none !important;
+    color: white;
+}
+
+body{
+    background-color: #0D1117;
+}
+
+header{
+    margin: 3vh 1vw;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: #161B22;
+    border-radius: 30px;
+    background-color: #161B22;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+}
+
+header:hover, section:hover{
+    box-shadow: 0px 0px 15px black;
+}
+
+.brand{
+    display: flex;
+    align-items: center;
+}
+
+img{
+    width: 2.5rem;
+    height: 2.5rem;
+    border: 2px solid black;
+    border-radius: 50%;
+}
+
+.name{
+    margin-left: 1vw;
+    font-size: 1.5rem;
+}
+
+.intro{
+    text-align: center;
+    margin-bottom: 2vh;
+    margin-top: 1vh;
+}
+
+.social a{
+    font-size: 1.5rem;
+    padding-left: 1vw;
+}
+
+.social a:hover, .brand:hover{
+    filter: invert(0.3);
+}
+
+section{
+    margin: 0vh 1vw;
+    margin-bottom: 10vh;
+    padding: 1vh 3vw;
+    display: flex;
+    flex-direction: column;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    border-radius: 20px;
+    background-color: #161B22 ;
+}
+
+li:nth-child(1){
+    padding: 1rem 1rem 0.5rem 1rem;
+}
+
+li:nth-child(n+1){
+    padding-left: 1rem;
+}
+
+li label{
+    padding-left: 0.5rem;
+}
+
+li{
+    padding-bottom: 0.5rem;
+}
+
+span{
+    margin-right: 0.5rem;
+    cursor: pointer;
+    user-select: none;
+    transition: transform 200ms ease-out;
+}
+
+span.active{
+    transform: rotate(90deg);
+    -ms-transform: rotate(90deg);	 /* for IE  */
+    -webkit-transform: rotate(90deg);/* for browsers supporting webkit (such as chrome, firefox, safari etc.). */
+    display: inline-block;
+}
+
+ul{
+    margin: 1vh 1vw 1vh 1vw;
+    padding: 0 0 0.5rem 0;
+    border: 2px solid black;
+    border-radius: 20px;
+    background-color: #1c2129;
+    overflow: hidden;
+}
+
+input[type="checkbox"]{
+    cursor: pointer;
+    user-select: none;
+}
+
+input[type="submit"] {
+    border-radius: 20px;
+    margin: 2vh auto 1vh auto;
+    width: 50%;
+    display: block;
+    height: 5.5vh;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    background-color: #0D1117;
+    font-size: 16px;
+    font-weight: 500;
+}
+
+input[type="submit"]:hover, input[type="submit"]:focus{
+    background-color: rgba(255, 255, 255, 0.068);
+    cursor: pointer;
+}
+
+@media (max-width: 768px){
+    input[type="submit"]{
+        width: 100%;
+    }
+}
+
+#treeview .parent {
+    position: relative;
+}
+
+#treeview .parent > ul {
+    display: none;
+}
+
+</style>
+</head>
+<body>
+  <!--© Designed and coded by @bipuldey19-Telegram-->
+    <header>
+      <div class="brand">
+        <img
+          src="https://telegra.ph/file/cc06d0c613491080cc174.png"
+          alt="logo"
+        />
+        <a href="https://t.me/anas_tayyar">
+          <h2 class="name">Qbittorrent Selection</h2>
+        </a>
+      </div>
+      <div class="social">
+        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+      </div>
+    </header>
+    <section>
+      <h2 class="intro">Select the files you want to download</h2>
+      <form action="{form_url}" method="POST">
+       {My_content}
+       <input type="submit" name="Select these files ;)">
+      </form>
+    </section>
+
+    <script>
+      $(document).ready(function () {
+        var tags = $("li").filter(function () {
+          return $(this).find("ul").length !== 0;
+        });
+
+        tags.each(function () {
+          $(this).addClass("parent");
+        });
+
+        $("body").find("ul:first-child").attr("id", "treeview");
+        $(".parent").prepend("<span>▶</span>");
+
+        $("span").click(function (e) {
+          e.stopPropagation();
+          e.stopImmediatePropagation();
+          $(this).parent( ".parent" ).find(">ul").toggle("slow");
+          if ($(this).hasClass("active")) $(this).removeClass("active");
+          else $(this).addClass("active");
+        });
+      });
+
+      if(document.getElementsByTagName("ul").length >= 10){
+      var labels = document.querySelectorAll("label");
+      //Shorting the file/folder names
+      labels.forEach(function (label) {
+        if (label.innerText.toString().split(" ").length >= 6) {
+          let FirstPart = label.innerText
+            .toString()
+            .split(" ")
+            .slice(0, 3)
+            .join(" ");
+          let SecondPart = label.innerText
+            .toString()
+            .split(" ")
+            .splice(-3)
+            .join(" ");
+          label.innerText = `${FirstPart}... ${SecondPart}`;
+        }
+        if (label.innerText.toString().split(".").length >= 6) {
+          let first = label.innerText
+            .toString()
+            .split(".")
+            .slice(0, 3)
+            .join(" ");
+          let second = label.innerText
+            .toString()
+            .split(".")
+            .splice(-3)
+            .join(".");
+          label.innerText = `${first}... ${second}`;
+        }
+      });
+     }
+    </script>
+
+<script>
+$('input[type="checkbox"]').change(function(e) {
+  var checked = $(this).prop("checked"),
+      container = $(this).parent(),
+      siblings = container.siblings();
+/*
+  $(this).attr('value', function(index, attr){
+     return attr == 'yes' ? 'noo' : 'yes';
+  });
+*/
+  container.find('input[type="checkbox"]').prop({
+    indeterminate: false,
+    checked: checked
+  });
+  function checkSiblings(el) {
+    var parent = el.parent().parent(),
+        all = true;
+    el.siblings().each(function() {
+      let returnValue = all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+      return returnValue;
+    });
+
+    if (all && checked) {
+      parent.children('input[type="checkbox"]').prop({
+        indeterminate: false,
+        checked: checked
+      });
+      checkSiblings(parent);
+    } else if (all && !checked) {
+      parent.children('input[type="checkbox"]').prop("checked", checked);
+      parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+      checkSiblings(parent);
+    } else {
+      el.parents("li").children('input[type="checkbox"]').prop({
+        indeterminate: true,
+        checked: false
+      });
+    }
+  }
+  checkSiblings(container);
+});
+</script>
+</body>
+</html>""", content_type="text/html")
 
 async def e404_middleware(app, handler):
 
@@ -693,11 +994,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2>", content_type="text/html")
             raise
     return middleware_handler
 
